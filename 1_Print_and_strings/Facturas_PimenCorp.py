ae51d0edfe05fa0a66dc9c88bcc18d34939032a0
Pimen0106 <josemapimtro@gmail.com>
2026-04-27 19:28:39 -0500

    ya sabe

--- 1_Print_and_strings/Facturas_PimenCorp.py
+++ 1_Print_and_strings/Facturas_PimenCorp.py
@@ -1,2 +1,8 @@
-print("Hola mundo jeje")
+""""
+OBJETIVO GENERAL DEL PROYECTO: 
+    GENERAR UNA FACTURA EN FORMATO PDF PARA UNA COMPAÑIA FICTICIA CON DATOS, VARIABLES Y CALCULACIONES DE IMPUESTOS AÑADIDOS.
+"""
+
+nombre = input("Nombre y apellido del cliente: ")
+fecha = input(f"Fecha de la compra: ")
 
